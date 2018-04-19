293d251c78bbde99a4576238f4c6b7141f0acfc2
AndyBaird <andy.baird@icloud.com>
2018-04-19 15:41:17 -0600

    section 5 lesson 43)

--- src/app.js
+++ src/app.js
@@ -1,157 +1,175 @@
-console.log("App.js is running");
-
-
-// const obj = {
-//     name: 'name',
-//     getName(){
-//         return this.name;
-//     }   
-// };
-
-//you lose 'this' context when used in functions or event handlers, therefore you must rebind like so
-//const getName = obj.getName.bind(obj);
-// const getName = obj.getName.bind({name: 'Andrew'});
-
-
-
 class IndecisionApp extends React.Component {
     constructor(props) {
-        super(props);
-        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-        this.handlePick = this.handlePick.bind(this);
-        this.state = {
-            options : ['thing one', 'thing two', 'thing three']
-        }
-    };
-    handleDeleteOptions(){
-        this.setState(() =>{
-            return {
-                options: []
-            }
-        })
+      super(props);
+      this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+      this.handlePick = this.handlePick.bind(this);
+      this.handleAddOption = this.handleAddOption.bind(this);
+      this.handleDeleteOption = this.handleDeleteOption.bind(this);
+      this.state = {
+        options: props.options
+      };
     }
     
-    handlePick(){        
-        const randomNum = Math.floor(Math.random() * this.state.options.length);
-        const option = this.state.options[randomNum];
-        alert(option)
-        // this.setState(() =>{
-
-        //     // return (
-                
-        //     // )
-        // });
+    handleDeleteOptions() {
+        this.setState(() =>({ options: [] }));
+        //returns an empty object
+        //const num = () => ({})
+        //returns a function body of undefined
+        //const num = () => {}
     }
-    render(){
-        const title = 'Indecision';
-        const subtitle = 'Put your life in the hands of a computer';
 
-        return (
-            <div>
-                <Header title={title} subtitle={subtitle} />
-                <Action 
-                    hasOptions={this.state.options.length > 0}
-                    handlePick={this.handlePick}
+    handleDeleteOption(optionToRemove){
+        this.setState((prevState) => ({
+            options: prevState.options.filter((option) => optionToRemove !== option)
+        }));
+    }
+
+    handlePick() {
+      const randomNum = Math.floor(Math.random() * this.state.options.length);
+      const option = this.state.options[randomNum];
+      alert(option);
+    }
+    handleAddOption(option) {
+      if (!option) {
+        return 'Enter valid value to add item';
+      } else if (this.state.options.indexOf(option) > -1) {
+        return 'This option already exists';
+      }
+
+      this.setState((prevState) =>({ 
+        options: prevState.options.concat(option) 
+      }));
+
+    }
+    render() {
+      const subtitle = 'Put your life in the hands of a computer';
+  
+      return (
+        <div>
+          <Header subtitle={subtitle} />
+          <Action
+            hasOptions={this.state.options.length > 0}
+            handlePick={this.handlePick}
+          />
+          <Options
+            options={this.state.options}
+            handleDeleteOptions={this.handleDeleteOptions} 
+            handleDeleteOption={this.handleDeleteOption}
+          />
+          <AddOption
+            handleAddOption={this.handleAddOption}
+          />
+        </div>
+      );
+    }
+  }
+
+  IndecisionApp.defaultProps= {
+      options: []
+  }
+  
+  const Header = (props) => {
+    return (
+        <div>
+          <h1>{props.title}</h1>
+          {props.subtitle && <h2>{props.subtitle}</h2>}
+        </div>
+      );
+  };
+  
+  Header.defaultProps = {
+      title: 'Indecision'
+  }
+
+  const Action = (props) => {
+    return (
+        <div>
+          <button
+            onClick={props.handlePick}
+            disabled={!props.hasOptions}
+          >
+            What should I do?
+          </button>
+        </div>
+      );
+  }
+  
+  const Options = (props) => {
+    return (
+        <div>
+          <button onClick={props.handleDeleteOptions}>Remove All</button>
+          {
+            props.options.map((option) => (
+                <Option 
+                key={option} 
+                optionText={option} 
+                handleDeleteOption={props.handleDeleteOption}
                 />
-                <Options 
-                    options={this.state.options}
-                    handleDeleteOptions={this.handleDeleteOptions}
-                    />
-                <AddOption />
-            </div>
-        )
+            ))
+          }
+        </div>
+      );
+  }
+
+  const Option = (props) => {
+    return (
+        <div>
+          {props.optionText}
+          <button 
+            onClick={(e) => {
+                props.handleDeleteOption(props.optionText)
+            }}
+          >
+          remove
+          </button>
+        </div>
+      );
+  }
+  
+  class AddOption extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleAddOption = this.handleAddOption.bind(this);
+      this.state = {
+        error: undefined
+      };
     }
-}
-
-class Header extends React.Component {
-    render(){
-        return (
-            <div>
-              <h1>{this.props.title}</h1>
-              <h2>{this.props.subtitle}</h2>            
-            </div>
-        );
-            
-    }
-}
-
-class Action extends React.Component {
-
-    render(){
-        return (
-            <div>
-                <button 
-                    onClick={this.props.handlePick}
-                    disabled={!this.props.hasOptions}>
-                    What Should I Do? 
-                </button>
-            </div>
-        );
-    }
-}
-
-class Options extends React.Component {
-    //overriding the constructor to fix binding
-    // constructor(props){
-    //     //must use props as first arg
-    //     super(props);
-    //     //fixes binding within class for all calls
-    //     this.handleRemoveAll = this.handleRemoveAll.bind(this);
-    // }
-
-    render(){
-        console.log(this.props);
-        return (
-            <div>
-                <button onClick={this.props.handleDeleteOptions}>Remove All</button>
-                {                    
-                    //key is a reserved word and not accessible as a prop
-                    this.props.options.map((option) => <Option key={option} optionText={option}/>)
-                }
-                <Option />
-                
-            </div>
-        );
-    }
-}
-
-class Option extends React.Component {
-    render(){
-        return (
-            <div>
-                {this.props.optionText}
-            </div>
-        );
-    }
-}
-
-
-
-class AddOption extends React.Component {
-    
     handleAddOption(e) {
-        e.preventDefault();
-    
-        const option = e.target.elements.option.value.trim();
-    
-        if(option){
-            alert(option);
-            // IndecisionApp.options.push(option);
-            // e.target.elements.option.value = '';
-            // ReactDOM.render(<IndecisionApp />, document.getElementById('app'));    
-        }
-    };
+      e.preventDefault();
+  
+      const option = e.target.elements.option.value.trim();
+      const error = this.props.handleAddOption(option);
 
-    render(){
-        return (
-            <div>
-                <form onSubmit={this.handleAddOption}>
-                    <input type="text" name="option"/> 
-                    <button>Add Option</button>
-                </form>
-            </div>
-        );
+      this.setState(() =>({ error }));
+
     }
-}
+    render() {
+      return (
+        <div>
+          {this.state.error && <p>{this.state.error}</p>}
+          <form onSubmit={this.handleAddOption}>
+            <input type="text" name="option" />
+            <button>Add Option</button>
+          </form>
+        </div>
+      );
+    }
+  }
 
+  //stateless functional component
+  //props get passed in as first argument
+  //you do not have access to "this" but you do have access to props
+  //they are faster - use them when you can, especially for presentational content
+//   const User = (props) => {
+//       return (
+//           <div>
+//               <p>Name: </p>
+//               <p>Age: </p>
+//           </div>
+//       );
+//   }
+  
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+  //the ability to pass in default props creates highly reusable components
+//   ReactDOM.render(<IndecisionApp options={["Options one", "Options Two"]}/>, document.getElementById('app'));
+  
